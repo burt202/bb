fea03bb80ff1a01bd8cd7c3c639df74f20ffdfdb
Aaron Burtnyk <burt202@gmail.com>
2022-04-11 23:47:30 +0100

    Add match tables

--- src/index.ts
+++ src/index.ts
@@ -90,12 +90,68 @@ initSqlJs({
   // luke ewert, reece ewert
 
   db.run(`
-    CREATE TABLE seasons (id text UNIQUE, season text UNIQUE, primary key (id));
-    CREATE TABLE match_types (id text UNIQUE, name text UNIQUE, rank int, primary key (id));
-    CREATE TABLE bots (id text UNIQUE, name text UNIQUE, primary key (id));
-    CREATE TABLE members (id text UNIQUE, name text UNIQUE, primary key (id));
-    CREATE TABLE season_bots (season_id text, bot_id text, match_type_id text, primary key (season_id, bot_id));
-    CREATE TABLE bot_members (bot_id text, member_id text, season_id text, primary key (member_id, season_id));
+    CREATE TABLE seasons (
+      id text UNIQUE,
+      season text UNIQUE,
+      primary key (id)
+    );
+
+    CREATE TABLE match_types (
+      id text UNIQUE,
+      name text UNIQUE,
+      rank int,
+      primary key (id)
+    );
+
+    CREATE TABLE bots (
+      id text UNIQUE,
+      name text UNIQUE,
+      primary key (id)
+    );
+
+    CREATE TABLE members (
+      id text UNIQUE,
+      name text UNIQUE,
+      primary key (id)
+    );
+
+    CREATE TABLE season_bots (
+      season_id text,
+      bot_id text,
+      match_type_id text,
+      primary key (season_id, bot_id),
+      foreign key (season_id) references seasons(id),
+      foreign key (bot_id) references bots(id),
+      foreign key (match_type_id) references match_types(id)
+    );
+
+    CREATE TABLE bot_members (
+      bot_id text,
+      member_id text,
+      season_id text,
+      primary key (member_id, season_id),
+      foreign key (bot_id) references bots(id),
+      foreign key (member_id) references members(id),
+      foreign key (season_id) references seasons(id)
+    );
+
+    CREATE TABLE matches (
+      id text UNIQUE,
+      ko bool,
+      match_type_id text,
+      winner_id text,
+      primary key (id),
+      foreign key (winner_id) references bots(id),
+      foreign key (match_type_id) references match_types(id)
+    );
+
+    CREATE TABLE match_competitors (
+      match_id text,
+      bot_id text,
+      primary key (match_id, bot_id),
+      foreign key (match_id) references matches(id),
+      foreign key (bot_id) references bots(id)
+    );
   `)
 
   db.run(`
@@ -122,16 +178,4 @@ initSqlJs({
       })
     })
   })
-
-  // const seasons = db.exec("SELECT * FROM seasons")
-  // console.log("seasons", seasons)
-
-  // const matchTypes = db.exec("SELECT * FROM match_types")
-  // console.log("matchTypes", matchTypes)
-
-  // const bots = db.exec("SELECT * FROM bots")
-  // console.log("bots", bots)
-
-  const seasonBots = db.exec("SELECT * FROM bot_members")
-  console.log("seasonBots", seasonBots)
 })
