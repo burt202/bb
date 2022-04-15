489af2772104a5e1c13d3bc5f83073b32e87d1bb
Aaron Burtnyk <burt202@gmail.com>
2022-04-15 21:12:34 +0100

    Add initial 2019 data

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -12,6 +12,7 @@ const data = {
   2015: require("../../data/2015.json") as RawSeason,
   2016: require("../../data/2016.json") as RawSeason,
   2018: require("../../data/2018.json") as RawSeason,
+  2019: require("../../data/2019.json") as RawSeason,
 }
 
 export const DbContext = React.createContext<DbInterface | null>(null)
