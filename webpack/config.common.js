980b21cedbd3d4d20408999d4fa4480cbf32d5c9
Aaron Burtnyk <burt202@gmail.com>
2022-04-16 19:33:33 +0100

    Add favicon

--- webpack/config.common.js
+++ webpack/config.common.js
@@ -33,6 +33,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
+        {from: "src/favicon.ico", to: "favicon.ico"},
         {from: "./node_modules/sql.js/dist/sql-wasm.wasm", to: "sql-wasm.wasm"},
         {from: "src/images/tick.svg", to: "tick.svg"},
         {from: "src/images/cross.svg", to: "cross.svg"},
