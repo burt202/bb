deeeceeaa8a2bf62fd3bcdace1180e7bdb2bc7eb
Aaron Burtnyk <aaron.burtnyk@candide.com>
2022-04-28 21:59:12 +0100

    Add scroll wrapper

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
-import {HashRouter, Routes, Route} from "react-router-dom"
+import {useLayoutEffect} from "react"
+import {HashRouter, Routes, Route, useLocation} from "react-router-dom"
 import Bot from "./pages/bot"
 import Country from "./pages/country"
 import Home from "./pages/home"
@@ -7,17 +8,33 @@ import Member from "./pages/member"
 import NotFound from "./pages/not-found"
 import Season from "./pages/season"
 
+interface Props {
+  children: JSX.Element
+}
+
+const Wrapper = ({children}: Props) => {
+  const location = useLocation()
+
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0)
+  }, [location.pathname])
+
+  return children
+}
+
 export default function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/season/:seasonId" element={<Season />} />
-        <Route path="/bot/:botId" element={<Bot />} />
-        <Route path="/member/:memberId" element={<Member />} />
-        <Route path="/country/:countryId" element={<Country />} />
-        <Route path="*" element={<NotFound title="Page Not Found" />} />
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/season/:seasonId" element={<Season />} />
+          <Route path="/bot/:botId" element={<Bot />} />
+          <Route path="/member/:memberId" element={<Member />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<NotFound title="Page Not Found" />} />
+        </Routes>
+      </Wrapper>
     </HashRouter>
   )
 }
