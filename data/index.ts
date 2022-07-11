038ed6286e09835bebc04994d3bec6721ce3b464
Aaron Burtnyk <aaron.burtnyk@candide.com>
2022-07-11 21:45:35 +0100

    Add more season 5 data

--- data/index.ts
+++ data/index.ts
@@ -3,11 +3,11 @@
 import {RawSeason} from "../src/js/types"
 
 export default {
-  // s01: require("./s01.json") as RawSeason,
-  // s02: require("./s02.json") as RawSeason,
-  // s03: require("./s03.json") as RawSeason,
-  // s04: require("./s04.json") as RawSeason,
-  // s05: require("./s05.json") as RawSeason,
+  s01: require("./s01.json") as RawSeason,
+  s02: require("./s02.json") as RawSeason,
+  s03: require("./s03.json") as RawSeason,
+  s04: require("./s04.json") as RawSeason,
+  s05: require("./s05.json") as RawSeason,
   s06: require("./s06.json") as RawSeason,
   s07: require("./s07.json") as RawSeason,
   s08: require("./s08.json") as RawSeason,
